7f2f09f6d7321dfc3755502e0b6f1cb190674d55
Skyler Greenman <sky.todd.green@gmail.com>
2020-07-07 16:51:51 -0600

    first commit; error in displaying images on web

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { PhotoService } from '../services/photo.service';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
-export class Tab2Page {
+/* ^ This Component decorator:
+  - specifies any class immediately below it (Tab2Page) as a component class
+  - specifies its metadata
 
-  constructor() {}
+  * selector - create and inserts this view (component & template) if angular finds the tag <app-tab2></app-tab2> in any other HTML files
+  * templateUrl - identifies the component's HTML template (file)
+  * providers (not shown) - array of services that component reies on  
+*/
 
+export class Tab2Page { 
+
+
+  //photos = this.photoService.photos;
+
+  constructor(public photoService: PhotoService) { }
+
+  ngOnInit() {
+    this.photoService.loadSaved();
+  }
+  
+
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
+
+  log(val) { 
+    console.log(Object.keys(val));
+    console.log(JSON.stringify(val));
+    if (val.hasOwnProperty("id")){
+      console.log(val.id);
+    }
+  }
+    
+    //"base64: " + val.base64 + "  webview: " + val.webviewPath);}
+  
+
+  
 }
